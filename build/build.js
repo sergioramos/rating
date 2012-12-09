80836a0aa29e284b0c54ce7859c7ba69798d4205
Sérgio Ramos <mail@sergioramos.me>
2012-12-09 18:35:52 +0000

    un-ignore build

---
+++ build/build.js
@@ -0,0 +1,381 @@
+/**
+ * Require the given path.
+ *
+ * @param {String} path
+ * @return {Object} exports
+ * @api public
+ */
+
+function require(p, parent, orig){
+  var path = require.resolve(p)
+    , mod = require.modules[path];
+
+  // lookup failed
+  if (null == path) {
+    orig = orig || p;
+    parent = parent || 'root';
+    throw new Error('failed to require "' + orig + '" from "' + parent + '"');
+  }
+
+  // perform real require()
+  // by invoking the module's
+  // registered function
+  if (!mod.exports) {
+    mod.exports = {};
+    mod.client = mod.component = true;
+    mod.call(this, mod, mod.exports, require.relative(path));
+  }
+
+  return mod.exports;
+}
+
+/**
+ * Registered modules.
+ */
+
+require.modules = {};
+
+/**
+ * Registered aliases.
+ */
+
+require.aliases = {};
+
+/**
+ * Resolve `path`.
+ *
+ * Lookup:
+ *
+ *   - PATH/index.js
+ *   - PATH.js
+ *   - PATH
+ *
+ * @param {String} path
+ * @return {String} path or null
+ * @api private
+ */
+
+require.resolve = function(path){
+  var orig = path
+    , reg = path + '.js'
+    , regJSON = path + '.json'
+    , index = path + '/index.js'
+    , indexJSON = path + '/index.json';
+
+  return require.modules[reg] && reg
+    || require.modules[regJSON] && regJSON
+    || require.modules[index] && index
+    || require.modules[indexJSON] && indexJSON
+    || require.modules[orig] && orig
+    || require.aliases[index];
+};
+
+/**
+ * Normalize `path` relative to the current path.
+ *
+ * @param {String} curr
+ * @param {String} path
+ * @return {String}
+ * @api private
+ */
+
+require.normalize = function(curr, path) {
+  var segs = [];
+
+  if ('.' != path.charAt(0)) return path;
+
+  curr = curr.split('/');
+  path = path.split('/');
+
+  for (var i = 0; i < path.length; ++i) {
+    if ('..' == path[i]) {
+      curr.pop();
+    } else if ('.' != path[i] && '' != path[i]) {
+      segs.push(path[i]);
+    }
+  }
+
+  return curr.concat(segs).join('/');
+};
+
+/**
+ * Register module at `path` with callback `fn`.
+ *
+ * @param {String} path
+ * @param {Function} fn
+ * @api private
+ */
+
+require.register = function(path, fn){
+  require.modules[path] = fn;
+};
+
+/**
+ * Alias a module definition.
+ *
+ * @param {String} from
+ * @param {String} to
+ * @api private
+ */
+
+require.alias = function(from, to){
+  var fn = require.modules[from];
+  if (!fn) throw new Error('failed to alias "' + from + '", it does not exist');
+  require.aliases[to] = from;
+};
+
+/**
+ * Return a require function relative to the `parent` path.
+ *
+ * @param {String} parent
+ * @return {Function}
+ * @api private
+ */
+
+require.relative = function(parent) {
+  var p = require.normalize(parent, '..');
+
+  /**
+   * lastIndexOf helper.
+   */
+
+  function lastIndexOf(arr, obj){
+    var i = arr.length;
+    while (i--) {
+      if (arr[i] === obj) return i;
+    }
+    return -1;
+  }
+
+  /**
+   * The relative require() itself.
+   */
+
+  function fn(path){
+    var orig = path;
+    path = fn.resolve(path);
+    return require(path, parent, orig);
+  }
+
+  /**
+   * Resolve relative to the parent.
+   */
+
+  fn.resolve = function(path){
+    // resolve deps by returning
+    // the dep in the nearest "deps"
+    // directory
+    if ('.' != path.charAt(0)) {
+      var segs = parent.split('/');
+      var i = lastIndexOf(segs, 'deps') + 1;
+      if (!i) i = 0;
+      path = segs.slice(0, i + 1).join('/') + '/deps/' + path;
+      return path;
+    }
+    return require.normalize(p, path);
+  };
+
+  /**
+   * Check if module is defined at `path`.
+   */
+
+  fn.exists = function(path){
+    return !! require.modules[fn.resolve(path)];
+  };
+
+  return fn;
+};require.register("component-domify/index.js", function(module, exports, require){
+
+/**
+ * Expose `parse`.
+ */
+
+module.exports = parse;
+
+/**
+ * Wrap map from jquery.
+ */
+
+var map = {
+  option: [1, '<select multiple="multiple">', '</select>'],
+  optgroup: [1, '<select multiple="multiple">', '</select>'],
+  legend: [1, '<fieldset>', '</fieldset>'],
+  thead: [1, '<table>', '</table>'],
+  tbody: [1, '<table>', '</table>'],
+  tfoot: [1, '<table>', '</table>'],
+  colgroup: [1, '<table>', '</table>'],
+  caption: [1, '<table>', '</table>'],
+  tr: [2, '<table><tbody>', '</tbody></table>'],
+  td: [3, '<table><tbody><tr>', '</tr></tbody></table>'],
+  th: [3, '<table><tbody><tr>', '</tr></tbody></table>'],
+  col: [2, '<table><tbody></tbody><colgroup>', '</colgroup></table>'],
+  _default: [0, '', '']
+};
+
+/**
+ * Parse `html` and return the children.
+ *
+ * @param {String} html
+ * @return {Array}
+ * @api private
+ */
+
+function parse(html) {
+  if ('string' != typeof html) throw new TypeError('String expected');
+  
+  // tag name
+  var m = /<([\w:]+)/.exec(html);
+  if (!m) throw new Error('No elements were generated.');
+  var tag = m[1];
+  
+  // body support
+  if (tag == 'body') {
+    var el = document.createElement('html');
+    el.innerHTML = html;
+    return [el.removeChild(el.lastChild)];
+  }
+  
+  // wrap map
+  var wrap = map[tag] || map._default;
+  var depth = wrap[0];
+  var prefix = wrap[1];
+  var suffix = wrap[2];
+  var el = document.createElement('div');
+  el.innerHTML = prefix + html + suffix;
+  while (depth--) el = el.lastChild;
+
+  return orphan(el.children);
+}
+
+/**
+ * Orphan `els` and return an array.
+ *
+ * @param {NodeList} els
+ * @return {Array}
+ * @api private
+ */
+
+function orphan(els) {
+  var ret = [];
+
+  while (els.length) {
+    ret.push(els[0].parentNode.removeChild(els[0]));
+  }
+
+  return ret;
+}
+
+});
+require.register("component-event/index.js", function(module, exports, require){
+
+/**
+ * Bind `el` event `type` to `fn`.
+ *
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @api public
+ */
+
+exports.bind = function(el, type, fn, capture){
+  if (el.addEventListener) {
+    el.addEventListener(type, fn, capture);
+  } else {
+    el.attachEvent('on' + type, fn);
+  }
+};
+
+/**
+ * Unbind `el` event `type`'s callback `fn`.
+ *
+ * @param {Element} el
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ * @api public
+ */
+
+exports.unbind = function(el, type, fn, capture){
+  if (el.removeEventListener) {
+    el.removeEventListener(type, fn, capture);
+  } else {
+    el.detachEvent('on' + type, fn);
+  }
+};
+});
+require.register("rating/src/template.js", function(module, exports, require){
+module.exports = "<div class='rating'><span>&#9733;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span><span>&#9734;</span></div>";
+});
+require.register("rating/src/rating.js", function(module, exports, require){
+var html = require('./template'),
+    create = require('domify'),
+    ev = require('event')
+
+module.exports = function (insert) {
+  var numbers = {49: 1, 50: 2, 51: 3, 52: 4, 53: 5};
+  var stars = create(html).pop();
+  var nodes = new Array();
+  var enabled = true;
+  var rating = 1;
+  
+  var render = function (i) {
+    nodes.forEach(function (node, y) {
+      if(y<i) node.innerHTML = '&#9733;';
+      else node.innerHTML = '&#9734;';
+    });
+  };
+  
+  insert(stars);
+  
+  Array.prototype.forEach.call(stars.childNodes, function (node, i) {
+    nodes.push(node);
+    
+    ev.bind(node, 'mouseover', function () {
+      if(!enabled) return;
+      render(i+1);
+    });
+    
+    ev.bind(node, 'click', function () {
+      if(!enabled) return;
+      rating = i+1;
+      render(i+1);
+    });
+  });
+  
+  ev.bind(stars, 'mouseout', function () {
+    if(!enabled) return;
+    render(rating);
+  });
+  
+  ev.bind(document, 'keydown', function (e) {
+    if(!enabled) return;
+    var tag = e.target.tagName.toLowerCase();
+    if(tag === 'input' || tag === 'textarea') return;
+    if(!numbers[e.keyCode]) return;
+    rating = numbers[e.keyCode];
+    render(rating);
+  });
+  
+  render(rating)
+
+  var returns = function () {
+    return rating;
+  };
+
+  returns.disable = function () {
+    enabled = false;
+  };
+
+  returns.enable = function () {
+    enabled = true;
+  };
+
+  return returns;
+};
+});
+require.alias("component-domify/index.js", "rating/deps/domify/index.js");
+
+require.alias("component-event/index.js", "rating/deps/event/index.js");
+
+require.alias("rating/src/rating.js", "rating/index.js");
